14462fec5639186e6b9836d661476185f7c8c204
SwapnilSalgar77 <85386134+SwapnilSalgar77@users.noreply.github.com>
2024-12-17 16:53:29 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Swapnil Generative AI Project',
+    version= '0.0.0',
+    author= 'Swapnil Salgar',
+    author_email= 'swapnil.mechworld@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
